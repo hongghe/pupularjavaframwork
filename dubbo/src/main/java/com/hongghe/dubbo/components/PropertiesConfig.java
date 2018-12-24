7b65f66a139d5335825c13524359e8a6c5dcd65b
hongghe <364930855@qq.com>
2018-12-24 20:59:24 +0800

    Other prokect of pupular.

---
+++ dubbo/src/main/java/com/hongghe/dubbo/components/PropertiesConfig.java
@@ -0,0 +1,14 @@
+package com.hongghe.dubbo.components;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hongghe 2018/12/24
+ */
+@Component
+@ConfigurationProperties(prefix = "com.hongghe")
+@PropertySource(value = "application.properties")
+public class PropertiesConfig {
+}
