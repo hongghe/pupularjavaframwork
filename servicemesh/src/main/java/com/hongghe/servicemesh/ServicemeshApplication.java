23c65f868fb529d850881d4bafc6a10df60844e8
hongghe <364930855@qq.com>
2019-01-03 14:34:02 +0800

    dubbo api consumer and provider setting.

---
+++ servicemesh/src/main/java/com/hongghe/servicemesh/ServicemeshApplication.java
@@ -0,0 +1,14 @@
+package com.hongghe.servicemesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicemeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicemeshApplication.class, args);
+	}
+
+}
+
