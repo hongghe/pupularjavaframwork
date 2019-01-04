27df7b3fcf34c90c62e96bb2c2c4f33989c0aec5
hongghe <364930855@qq.com>
2019-01-04 21:09:01 +0800

    Add mapper.

---
+++ mybatis/src/main/java/com/hongghe/mybatis/annotation/FruitClazz.java
@@ -0,0 +1,7 @@
+package com.hongghe.mybatis.annotation;
+
+/**
+ * @author: hongghe @date: 2019-01-04 18:32
+ */
+public @interface FruitClazz {
+}
---
+++ mybatis/src/main/java/com/hongghe/mybatis/interceptor/FruitInterceptor.java
@@ -0,0 +1,7 @@
+package com.hongghe.mybatis.interceptor;
+
+/**
+ * @author: hongghe @date: 2019-01-04 18:29
+ */
+public class FruitInterceptor {
+}
