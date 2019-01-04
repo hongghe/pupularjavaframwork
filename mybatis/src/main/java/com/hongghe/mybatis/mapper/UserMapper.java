5fa38cb34924a2a170ee36262b6a13337f930384
hongghe <364930855@qq.com>
2019-01-04 18:24:05 +0800

    Add mapper.

---
+++ mybatis/src/main/java/com/hongghe/mybatis/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.hongghe.mybatis.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author: hongghe @Date: 2019-01-04 18:19
+ */
+@Mapper
+public interface UserMapper {
+
+    
+}
