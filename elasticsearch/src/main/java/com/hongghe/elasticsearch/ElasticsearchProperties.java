1efbde5a49131a0b957b04d712acf3abc04491c9
hongghe <364930855@qq.com>
2019-01-10 10:27:49 +0800

    spring mvc setting.

---
+++ elasticsearch/src/main/java/com/hongghe/elasticsearch/ElasticsearchProperties.java
@@ -0,0 +1,7 @@
+package com.hongghe.elasticsearch;
+
+/**
+ * @author: hongghe @date: 2019-01-10 10:25
+ */
+public class ElasticsearchProperties {
+}
---
+++ elasticsearch/src/main/java/com/hongghe/elasticsearch/ElasticsearchWebConfiguration.java
@@ -0,0 +1,7 @@
+package com.hongghe.elasticsearch;
+
+/**
+ * @author: hongghe @date: 2019-01-10 10:24
+ */
+public class ElasticsearchWebConfiguration {
+}
