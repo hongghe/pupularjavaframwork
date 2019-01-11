2d9e16345922822a297a91d46811038d22873308
hongghe <364930855@qq.com>
2019-01-11 15:18:35 +0800

    spring cloud alibaba.

---
+++ ablibaba-cloud/src/main/java/com/hongghe/ablibabacloud/AblibabaCloudApplication.java
@@ -0,0 +1,14 @@
+package com.hongghe.ablibabacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AblibabaCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AblibabaCloudApplication.class, args);
+    }
+
+}
+
---
+++ sentinel/src/main/java/com/hongghe/sentinel/SentinelApplication.java
@@ -0,0 +1,14 @@
+package com.hongghe.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApplication.class, args);
+    }
+
+}
+
