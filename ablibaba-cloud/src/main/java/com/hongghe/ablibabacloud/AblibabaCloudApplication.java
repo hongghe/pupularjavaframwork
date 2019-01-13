fa8ab22c7f5c1bf0a511efb1e7a400c1fb4da81d
hongghe <364930855@qq.com>
2019-01-13 14:16:56 +0800

    Alibaba cloud.

--- ablibaba-cloud/src/main/java/com/hongghe/ablibabacloud/AblibabaCloudApplication.java
+++ ablibaba-cloud/src/main/java/com/hongghe/ablibabacloud/AblibabaCloudApplication.java
@@ -2,9 +2,20 @@ package com.hongghe.ablibabacloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AblibabaCloudApplication {
+
+    @Bean
+    public AblibabaCloudInitializer ablibabaCloudInitializer() {
+        return new AblibabaCloudInitializer();
+    }
+
+    @Bean
+    public AblibabaCloudProperties ablibabaCloudProperties() {
+        return new AblibabaCloudProperties();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AblibabaCloudApplication.class, args);
---
+++ ablibaba-cloud/src/main/java/com/hongghe/ablibabacloud/AblibabaCloudProperties.java
@@ -0,0 +1,7 @@
+package com.hongghe.ablibabacloud;
+
+/**
+ * @author: hongghe @date: 2019-01-13 13:23
+ */
+public class AblibabaCloudProperties {
+}
