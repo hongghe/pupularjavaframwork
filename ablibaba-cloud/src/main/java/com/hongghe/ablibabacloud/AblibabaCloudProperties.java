315145a12dc96fc6b6b944eaaed83f30f313e677
hongghe <364930855@qq.com>
2019-01-13 16:42:43 +0800

    The message

--- ablibaba-cloud/src/main/java/com/hongghe/ablibabacloud/AblibabaCloudProperties.java
+++ ablibaba-cloud/src/main/java/com/hongghe/ablibabacloud/AblibabaCloudProperties.java
@@ -1,7 +1,14 @@
 package com.hongghe.ablibabacloud;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
 /**
  * @author: hongghe @date: 2019-01-13 13:23
  */
+@Component
+@PropertySource(value = "classpath:/application.properties")
+@ConfigurationProperties(prefix = "alibabaclouddemo")
 public class AblibabaCloudProperties {
 }
