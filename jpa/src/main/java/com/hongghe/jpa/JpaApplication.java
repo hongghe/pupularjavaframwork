63f0d29648ffb307829585d6d79c6908302fb883
hongghe <364930855@qq.com>
2019-01-14 15:52:22 +0800

    Spring MVC AND WEB setting.

--- jpa/src/main/java/com/hongghe/jpa/JpaApplication.java
+++ jpa/src/main/java/com/hongghe/jpa/JpaApplication.java
@@ -1,10 +1,14 @@
 package com.hongghe.jpa;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
+@EnableSwagger2Doc
 @ServletComponentScan
 @SpringBootApplication
 public class JpaApplication {
