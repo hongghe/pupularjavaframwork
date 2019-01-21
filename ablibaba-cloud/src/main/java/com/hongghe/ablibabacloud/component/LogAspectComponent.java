d0cf49de81bac0699f53c820222be502e12b57bf
hongghe <364930855@qq.com>
2019-01-21 16:48:47 +0800

    test

--- ablibaba-cloud/src/main/java/com/hongghe/ablibabacloud/component/LogAspectComponent.java
+++ ablibaba-cloud/src/main/java/com/hongghe/ablibabacloud/component/LogAspectComponent.java
@@ -11,4 +11,23 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LogAspectComponent {
+
+   // 定义一个 Pointcut, 使用 切点表达式函数 来描述对哪些 Join point 使用 advise.
+   @Pointcut("@annotation(com.hongghe.ablibabacloud.AuthorizationAnnotation)")
+   public void pointcut() {
+   }
+
+   // 定义 advise
+   @Around("pointcut()")
+   public Object checkAuth(ProceedingJoinPoint joinPoint) throws Throwable {
+       HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+               .getRequest();
+
+       // 检查用户所传递的 token 是否合法
+       String token = getUserToken(request);
+       if (!token.equalsIgnoreCase("123456")) {
+           return "错误, 权限不合法!";
+       }
+
+       return joinPoint.proceed();
 }
