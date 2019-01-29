d2309d10688e46189237d03796a8f9451cf23a3f
hongghe <364930855@qq.com>
2019-01-29 10:08:40 +0800

    configuration of project.

--- kafka/src/main/java/com/hongghe/kafka/controller/ExceptionController.java
+++ kafka/src/main/java/com/hongghe/kafka/controller/ExceptionController.java
@@ -4,6 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author: hongghe @date: 2019-01-05 11:09
  */
@@ -11,4 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/exception")
 public class ExceptionController {
+
+    @RequestMapping(value = "hello", produces = "text/plain")
+    public void hello(HttpServletRequest request , HttpServletResponse response) throws IOException {
+        response.sendRedirect("/hello.html");
+    }
 }
