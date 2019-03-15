3ff37c2ab9215074aaa7df527d22d82ca7da8a7d
hongghe <364930855@qq.com>
2019-03-15 13:55:36 +0800

    Annotation of result.

---
+++ netty/src/main/java/com/hongghe/netty/NettyProperties.java
@@ -0,0 +1,7 @@
+package com.hongghe.netty;
+
+/**
+ * @author: hongghe @date: 2019-02-28 14:57
+ */
+public class NettyProperties {
+}
---
+++ netty/src/main/java/com/hongghe/netty/component/NettyServerListener.java
@@ -0,0 +1,10 @@
+package com.hongghe.netty.component;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: hongghe @date: 2019-02-28 14:59
+ */
+@Component
+public class NettyServerListener {
+}
